f806d5ec1d1737bcf92093908e2f49f7557d68b4
Bernard Mordan <bernardmordan@gmail.com>
2014-09-16 11:13:42 +0100

    module for BikeContainer created. Passing tests for DockingStation class which uses the module

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,30 +1,11 @@
+require_relative 'bike_container'
+
 class DockingStation
 
-	DEFAULT_CAPACITY = 10
+	include BikeContainer
     
     def initialize(options = {})
-    	@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
-    	@bikes = []
-    end
-    	
-    def dock(bike)
-      raise "Fuck the Station is full" if full?
-      @bikes << bike
+    	self.capacity = options.fetch(:capacity, capacity)
     end
 
-    def release(bike)
-      @bikes.delete(bike)
-    end
-
-	def bike_count
-      @bikes.count
-	end
-
-	def full?
-		bike_count == @capacity
-	end	
-
-	def available_bikes
-		@bikes.reject {|bike| bike.broken? }
-	end
 end
